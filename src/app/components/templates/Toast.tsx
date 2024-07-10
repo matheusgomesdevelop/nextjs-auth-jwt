b81a81fdb2153de0ca39dbbc32953c3158487184
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-07-10 10:35:41 -0300

    Ajustando estilos do toast

--- src/app/components/templates/Toast.tsx
+++ src/app/components/templates/Toast.tsx
@@ -18,13 +18,6 @@ const Toast: React.ForwardRefRenderFunction<ToastRef, ToastProps> = (props, ref)
         <PrToast.Toast
             ref={toastRef}
             pt={{
-                root: {
-                    className: 'min-w-96 opacity-90',
-                    style: {
-                        right: '30px',
-                    },
-                },
-
                 message: ({ state, index }: PrToast.ToastPassThroughMethodOptions & { index: number }) => ({
                     className: classNames('my-4 rounded-md w-full', {
                         'bg-blue-100 border-solid border-0 border-l-4 border-blue-500 text-blue-700':
@@ -55,6 +48,12 @@ const Toast: React.ForwardRefRenderFunction<ToastRef, ToastProps> = (props, ref)
                 },
                 text: {
                     className: 'text-base font-normal flex flex-col flex-1 grow shrink ml-4',
+                },
+                root: {
+                    className: 'min-w-96 opacity-90',
+                    style: {
+                        right: '30px',
+                    },
                 },
             }}
             {...props}
